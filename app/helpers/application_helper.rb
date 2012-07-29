ef88dcb232b5a05a57ba5f79f1e0e47841615e0f
Erik M Jacobs <erik@jumpshipservices.co>
2012-07-29 19:52:13 -0400

    Starting to get ready for gym administration.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,8 @@ module ApplicationHelper
   def nav_generator
     if admin_signed_in?
       render :partial => "shared/admin_navigation"
+    else
+      render :partial => "shared/login_navigation"
     end
   end
 
