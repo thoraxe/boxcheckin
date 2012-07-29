5ed64d9785d8a99ad8d4e12922ab42687d2b0da6
Erik M Jacobs <erik@jumpshipservices.co>
2012-07-29 17:13:37 -0400

    Initial commit of boxcheckin.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Boxcheckin::Application.config.secret_token = 'd3fed719b2e210d1cc4720f56a70a0d72aa14990081aa28df51bb8b495c4a9f1bafae8e3f2efef4d9fdca0c0e73d79fad0cde2e80ce010b5a4277664873d45a5'
