5d98df505076224b1c97645ff11ff94793d70848
Erik M Jacobs <erik@jumpshipservices.co>
2012-07-29 21:35:39 -0400

    Updated admin navigation and completed destroy action for boxes.

--- app/controllers/admins/boxes_controller.rb
+++ app/controllers/admins/boxes_controller.rb
@@ -18,13 +18,39 @@ class Admins::BoxesController < ApplicationController
     @box.password = string
     @box.password_confirmation = string
 
-    if @box.save!
+    if @box.save
       flash[:success] = "Box created successfully!"
       redirect_to admin_root_path
     else
       flash[:error] = "There was a problem with your submission."
       render :action => :new
     end
+  end
+
+  def edit
+    @box = Box.find(params[:id])
+  end
+
+  def update
+    @box = Box.find(params[:id])
+    @box.update_attributes(params[:box])
+    if @box.save
+      flash[:success] = "Box updated successfully!"
+      redirect_to admins_boxes_path
+    else
+      flash[:error] = "There was a problem with your submission."
+      render :action => :edit
+    end
+  end
+
+  def destroy
+    @box = Box.find(params[:id])
+    if @box.destroy
+      flash[:success] = "Box deleted successfully!"
+    else
+      flash[:error] = "There was some kind of problem."
+    end
+    redirect_to admins_boxes_path
   end
 
 end
