bf95d938512adb3ab4f894771a377d5b52f0313a
Erik M Jacobs <erik@jumpshipservices.co>
2012-07-30 12:58:50 -0400

    Added class hash to iterate over days

--- app/controllers/boxes_controller.rb
+++ app/controllers/boxes_controller.rb
@@ -2,6 +2,7 @@ class BoxesController < ApplicationController
   before_filter :authenticate_box!
 
   def index
+    @box_classes = current_box.box_classes
   end
 
 end
--- app/models/box_class.rb
+++ app/models/box_class.rb
@@ -1,3 +1,16 @@
 class BoxClass < ActiveRecord::Base
   belongs_to :box
+
+  def days_hash
+    days = {}
+    days[:monday] = self.monday
+    days[:tuesday] = self.tuesday
+    days[:wednesday] = self.wednesday
+    days[:thursday] = self.thursday
+    days[:friday] = self.friday
+    days[:saturday] = self.saturday
+    days[:sunday] = self.sunday
+
+    days
+  end
 end
