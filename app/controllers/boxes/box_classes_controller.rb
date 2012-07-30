2882514cbc056cd98dab304e31f317213a81198d
Erik M Jacobs <erik@jumpshipservices.co>
2012-07-30 11:40:01 -0400

    Box class add works.

--- app/controllers/boxes/box_classes_controller.rb
+++ app/controllers/boxes/box_classes_controller.rb
@@ -5,4 +5,16 @@ class Boxes::BoxClassesController < ApplicationController
     @box_class = BoxClass.new
   end
 
+  def create
+    @box_class = BoxClass.new(params[:box_class])
+
+    if current_box.box_classes << @box_class
+      flash[:success] = "Class added successfully."
+      redirect_to box_root_path
+    else
+      flash[:error] = "There was a problem with your submission."
+      render :action => :new
+    end
+  end
+
 end
