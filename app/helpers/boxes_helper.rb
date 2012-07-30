e8aeef485b3adc14b9112c7bbea1c6d5281a0d8b
Erik M Jacobs <erik@jumpshipservices.co>
2012-07-30 13:42:32 -0400

    Working on views.

--- app/helpers/boxes_helper.rb
+++ app/helpers/boxes_helper.rb
@@ -1,2 +1,6 @@
 module BoxesHelper
+  def delete_class_link(box_class)
+    link_to "Delete Class", boxes_box_class_path(box_class.id), {:method => :delete, :class => "btn btn-mini btn-danger", 
+      :confirm => "Really delete this class (#{BoxClass::DAYS[box_class.day][0]} #{box_class.start_time.strftime("%I:%M%p")})?"}
+  end
 end
