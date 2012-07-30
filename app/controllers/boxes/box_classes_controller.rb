f4bab6b1220adf3d9e6dc201b34b1e2859e53bbb
Erik M Jacobs <erik@jumpshipservices.co>
2012-07-30 13:45:21 -0400

    Destroy box class.

--- app/controllers/boxes/box_classes_controller.rb
+++ app/controllers/boxes/box_classes_controller.rb
@@ -17,4 +17,18 @@ class Boxes::BoxClassesController < ApplicationController
     end
   end
 
+  def destroy
+    @box_class = BoxClass.find(params[:id])
+    if @box_class.box_id == current_box.id
+      if @box_class.destroy
+        flash[:success] = "Class deleted successfully."
+      else
+        flash[:error] = "There was a problem deleting."
+      end
+    else
+      flash[:error] = "There was a problem deleting."
+    end
+    redirect_to box_root_path
+  end
+
 end
