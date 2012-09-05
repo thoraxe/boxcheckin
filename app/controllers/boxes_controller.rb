3f5908adc883cccea81cee27e1624f7f0c9b464c
Erik M Jacobs <erik@jumpshipservices.co>
2012-09-05 10:49:18 -0400

    Updated the navigation and some corresponding stuffs.

--- app/controllers/boxes_controller.rb
+++ app/controllers/boxes_controller.rb
@@ -2,13 +2,6 @@ class BoxesController < ApplicationController
   before_filter :authenticate_box!
 
   def index
-    @monday = BoxClass.monday(current_box.id)
-    @tuesday = BoxClass.tuesday(current_box.id)
-    @wednesday = BoxClass.wednesday(current_box.id)
-    @thursday = BoxClass.thursday(current_box.id)
-    @friday = BoxClass.friday(current_box.id)
-    @saturday = BoxClass.saturday(current_box.id)
-    @sunday = BoxClass.sunday(current_box.id)
   end
 
 end
